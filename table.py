e7aa05399d0f1f03b989f6159927f4ae4421fe90
MAES-MAES <108370325+MAES-MAES@users.noreply.github.com>
2022-06-30 15:21:35 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,79 @@
+
+from calendar import day_abbr
+from docx import Document
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+doc = Document('/Users/rustammirzoev/Desktop/Rustam_super.docx')
+
+
+list_ = [
+    {
+        "type": "filled",
+        "data": {
+
+            'name': "Наименование работ",'units': 'Единица измерения',"count": "100","price": "100","total": "1000",
+        }
+    },
+]
+
+
+table1 = doc.tables[0]
+
+for datas in list_[0]['data']:
+    
+    table1.rows[1].cells[1].text = datas
+   
+    print(datas)
+    
+    
+   
+
+
+
+
+
+
+
+    
+
+doc.save('demo2.docx')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list2=[
+
+    {
+        "type": "subtitle",
+        "data": {
+            "title": "Подзаголовок",
+        }
+    },
+
+]
+
+list3=[
+    {
+        "type": "total",
+        "data": {
+            "value": "2324 р."
+        }
+    }
+]
